0db460514cb6c3aeef199bc1ff8b990d189b9880
KHAIRUL ISLAM <khairulislam38@gmail.com>
2022-11-28 22:35:13 +0600

    update

--- src/components/Home/HeaderSection/HeaderSection.jsx
+++ src/components/Home/HeaderSection/HeaderSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const HeaderSection = () => {
   return (
@@ -7,6 +7,15 @@ const HeaderSection = () => {
         <h1 className="text-3xl font-bold">
           Amazing Goa Flight Inclusive Deal 4N
         </h1>
+        <div className="flex items-center mt-1 gap-1">
+          <p className="bg-[#26B5A9] px-2 py-0.5 text-white text-xs font-semibold rounded">
+            4N/5D
+          </p>
+          <p className="bg-black text-white text-xs font-semibold px-2 py-0.5 rounded">
+            Flexi Package
+          </p>
+          <p className="font-semibold text-gray-400 ml-1">4N Goa</p>
+        </div>
       </div>
     </div>
   );
